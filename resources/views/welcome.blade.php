8cfd3df827fd6780bed905312634a2476e67da4c
Jonathan <jonlillywebdev@gmail.com>
2020-05-11 10:49:00 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Selu Tech</title>
+
+        <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}" />
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <nav>
+                    <h1>Selu <span>technologies</span></h1>
+                    
+
+                    <ul>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+
+                </nav>
+            </header>
+
+            <section id="splash">
+                <div id="ctaWrapper">
+                    <div id="ctaLeftDiv">
+                        <h1>Big websites</h1>
+                        <h2>for small business</h2>
+                    </div>
+                    <div id="ctaRightDiv">
+                        
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+        
+    </body>
+</html>
